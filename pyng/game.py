6171e3425e377889c042b5485aea093d92a2145e
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2020-11-16 12:10:34 +0100

    add gameobjects collides

--- pyng/game.py
+++ pyng/game.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+from random import randint
 from pyng import settings
 
 
@@ -9,11 +10,12 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = False
         self.framerate = 60
+        self.boundaries = self._get_boundaries()
         self.game_objects = []
+        self.background = Background(self.screen)
         self.paddle1 = Paddle(settings.PADDLE_COLORNAME, settings.PADDLE_BOTTOM_POS)
         self.paddle2 = Paddle(settings.PADDLE_COLORNAME, settings.PADDLE_TOP_POS)
         self.ball = Ball()
-        self.background = Background(self.screen)
 
         self._load()
 
@@ -24,41 +26,76 @@ class Game:
         self.game_objects.append(self.ball)
 
     def _get_boundaries(self):
-        pass
+        bounds = {}
+        bounds["walls"] = {}
+        bounds["walls"]["left"] = pygame.Rect((0, 0), (5, 650))
+        bounds["walls"]["right"] = pygame.Rect((445, 0), (5, 650))
+        bounds["goals"] = {}
+        bounds["goals"]["top"] = pygame.Rect((0, 0), (450, 5))
+        bounds["goals"]["bottom"] = pygame.Rect((0, 645), (450, 5))
+        return bounds
 
     def _get_delta_time(self, last_time):
-        return (time.time() - last_time) * 60
+        return ((time.time() - last_time) * 60, time.time())
+
+    def _check_events(self, events):
+        for e in events:
+            if e.type == pygame.QUIT:
+                self.running = False
+
+    def _check_collides(self):
+        # paddles collides
+        if self.paddle1.position.collidedict(self.boundaries["walls"], 1):
+            self.paddle1.move_reverse()
+        if self.paddle2.position.collidedict(self.boundaries["walls"], 1):
+            self.paddle2.move_reverse()
+
+        # ball collides
+        if self.ball.position.collidedict(self.boundaries["walls"], 1):
+            self.ball.bounce()
+        if self.ball.position.collidedict(self.boundaries["goals"], 1):
+            self.ball.bounce()
+        if self.ball.position.colliderect(self.paddle1.position):
+            self.ball.bounce()
+        if self.ball.position.colliderect(self.paddle2.position):
+            self.ball.bounce()
+
+    def _check_keypressed(self, keys):
+        if keys[pygame.K_LEFT]:
+            self.paddle1.move_left()
+        if keys[pygame.K_RIGHT]:
+            self.paddle1.move_right()
+        if keys[pygame.K_q]:
+            self.paddle2.move_left()
+        if keys[pygame.K_d]:
+            self.paddle2.move_right()
+
+    def _update(self, delta_time):
+        for obj in self.game_objects:
+            obj.update(delta_time)
+
+    def _display(self):
+        for obj in self.game_objects:
+            self.screen.blit(obj.surface, obj.position)
+
+        # DEBUG show bounds
+        for bound_type in self.boundaries:
+            for bound in self.boundaries[bound_type]:
+                pygame.draw.rect(
+                    self.screen, (255, 0, 0), self.boundaries[bound_type][bound], 1
+                )
 
     def run(self):
         self.running = True
         last_time = time.time()
+        self.ball.serve()
         while self.running:
-            delta_time = self._get_delta_time(last_time)
-            last_time = time.time()
-
-            # handle window exit event
-            for e in pygame.event.get():
-                if e.type == pygame.QUIT:
-                    self.running = False
-
-            # handle key pressed
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_LEFT]:
-                self.paddle1.move_left()
-            if keys[pygame.K_RIGHT]:
-                self.paddle1.move_right()
-            if keys[pygame.K_q]:
-                self.paddle2.move_left()
-            if keys[pygame.K_d]:
-                self.paddle2.move_right()
-
-            # update loop
-            for obj in self.game_objects:
-                obj.update(delta_time)
-
-            # display loop
-            for obj in self.game_objects:
-                self.screen.blit(obj.surface, obj.position)
+            delta_time, last_time = self._get_delta_time(last_time)
+            self._check_keypressed(pygame.key.get_pressed())
+            self._check_events(pygame.event.get())
+            self._check_collides()
+            self._update(delta_time)
+            self._display()
 
             pygame.display.update()
             self.clock.tick(self.framerate)
@@ -86,14 +123,32 @@ class Background(GameObject):
 
 
 class Ball(GameObject):
+
+    MAX_VELOCITY = 5
+
     def __init__(self):
         super().__init__(pygame.Surface(settings.BALL_SIZE), settings.BALL_START_POS)
         self.color = pygame.color.THECOLORS[settings.BALL_COLORNAME]
         self.surface.set_colorkey((0, 0, 0))
+        self.x_velocity = 0
+        self.y_velocity = 0
+        self.angle = 0
         pygame.draw.circle(self.surface, self.color, (5, 5), 5)
 
+    def serve(self):
+        if randint(0, 1):
+            self.y_velocity = self.MAX_VELOCITY
+        else:
+            self.y_velocity = -self.MAX_VELOCITY
+
+    def bounce(self):
+        self.x_velocity = -self.x_velocity
+        self.y_velocity = -self.y_velocity
+
     def update(self, delta_time):
-        pass
+        self.position = self.position.move(
+            self.x_velocity * delta_time, self.y_velocity * delta_time
+        )
 
 
 class Paddle(GameObject):
@@ -115,6 +170,10 @@ class Paddle(GameObject):
     def move_right(self):
         if abs(self.x_velocity) <= self.MAX_X_VELOCITY:
             self.x_velocity += self.INC_X_VELOCITY_FACTOR
+
+    def move_reverse(self):
+        if abs(self.x_velocity) > self.INC_X_VELOCITY_FACTOR:
+            self.x_velocity = -self.x_velocity
 
     def decrease_x_velocity(self):
         # decrease velocity if it's in movement
