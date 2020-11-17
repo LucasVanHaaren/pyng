3ffca784d29f98f84cef28686f265d990778f961
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2020-11-17 18:12:42 +0100

    add attribute to define goals

--- pyng/map.py
+++ pyng/map.py
@@ -1,18 +1,19 @@
 import pygame as pg
 import pyng.settings as s
+from pyng.paddle import PaddleSide
 
 WALL_WIDTH = 5
 
 
 class Wall(pg.sprite.Sprite):
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, opponent=None):
         super().__init__()
         self.screen = pg.display.get_surface()
         self.screen_rect = self.screen.get_rect()
         self.image = pg.Surface([width, height]).convert()
-        self.image.fill(pg.Color("red3"))
+        # self.image.fill(pg.Color("red3"))
         self.rect = self.image.get_rect()
-
+        self.opponent = opponent
         self.rect.x = x
         self.rect.y = y
 
@@ -26,8 +27,14 @@ def get_side_boundaries(screen_rect):
 
 def get_goal_boundaries(screen_rect):
     bounds = []
-    bounds.append(Wall(0, 0, screen_rect.width, WALL_WIDTH))
+    bounds.append(Wall(0, 0, screen_rect.width, WALL_WIDTH, opponent=PaddleSide.BOTTOM))
     bounds.append(
-        Wall(0, screen_rect.height - WALL_WIDTH, screen_rect.width, WALL_WIDTH)
+        Wall(
+            0,
+            screen_rect.height - WALL_WIDTH,
+            screen_rect.width,
+            WALL_WIDTH,
+            opponent=PaddleSide.TOP,
+        )
     )
     return bounds
