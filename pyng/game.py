29b28d52804c2eb9107cc5ff34d4286503f88fc5
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2020-11-26 22:25:26 +0100

    add abstraction with context super class

--- pyng/game.py
+++ pyng/game.py
@@ -1,19 +1,16 @@
 import time
 import pygame as pg
 import pyng.settings as s
+
+from pyng.context import Context
 from pyng.paddle import Paddle, PaddleSide
 from pyng.ball import Ball
 from pyng.ui import BackgroundUI, ScoreUI
 
 
-class Game:
+class Game(Context):
     def __init__(self, screen):
-        self.screen = screen
-        self.screen_rect = screen.get_rect()
-        self.clock = pg.time.Clock()
-        self.running = False
-        self.framerate = 60
-
+        super().__init__(screen)
         self.background_ui = BackgroundUI("cadetblue", "white")
         self.score_ui = ScoreUI("white")
 
@@ -33,31 +30,19 @@ class Game:
         self.paddles.add(self.paddle1, self.paddle2)
 
     def run(self):
-        self.running = True
-        last_time = time.time()
-
+        super().run()
         while self.running:
-            delta_time, last_time = self._get_delta_time(last_time)
+            self._compute_delta_time()
+            self._handle_events(pg.event.get())
+            self._handle_keypressed(pg.key.get_pressed())
 
-            for e in pg.event.get():
-                if e.type == pg.QUIT:
-                    self.running = False
-
-            keys = pg.key.get_pressed()
-            if keys[pg.K_LEFT]:
-                self.paddle1.move_left()
-            if keys[pg.K_RIGHT]:
-                self.paddle1.move_right()
-            if keys[pg.K_a]:
-                self.paddle1.score += 1
-                self.score_ui.set_score([self.paddle1.score, self.paddle2.score])
-
-            self.game_objects.update(delta_time)
-
+            self.game_objects.update(self.delta_time)
             dirty = self.game_objects.draw(self.screen)
             pg.display.update(dirty)
-            self.clock.tick(self.framerate)
+            self._tick()
 
-    def _get_delta_time(self, last_time):
-        """Compute delta_time, return delta time and last time"""
-        return ((time.time() - last_time) * 60, time.time())
+    def _handle_keypressed(self, keys):
+        if keys[pg.K_LEFT]:
+            self.paddle1.move_left()
+        if keys[pg.K_RIGHT]:
+            self.paddle1.move_right()
